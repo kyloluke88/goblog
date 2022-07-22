4629c122a4470d39b420737ecb3df1c7eb54c94f
luke <kyloluke88@gmail.com>
2022-07-22 18:02:47 +0800

    blog create page

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 
 	"github.com/gorilla/mux"
 )
+// router := mux.NewRouter()    包级别的变量声明时不能使用 := 语法
+var router = mux.NewRouter()
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog！</h1>")
@@ -37,6 +39,57 @@ func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "创建新的文章")
 }
 
+func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
+    html := `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>创建文章 —— 我的技术博客</title>
+</head>
+<body>
+    <form action="%s" method="post">
+        <p><input type="text" name="title"></p>
+        <p><textarea name="body" cols="30" rows="10"></textarea></p>
+        <p><button type="submit">提交</button></p>
+    </form>
+</body>
+</html>
+`
+    storeURL, _ := router.Get("articles.store").URL()
+    fmt.Fprintf(w, html, storeURL)
+}
+
+func main() {
+
+	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
+	router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
+
+	router.HandleFunc("/articles/{id:[0-9]+}", articlesShowHandler).Methods("GET").Name("articles.show")
+	router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
+	router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
+	router.HandleFunc("/articles/create", articlesCreateHandler).Methods("GET").Name("articles.create")
+
+
+
+	// 自定义 404 页面
+	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
+
+	// 中间件：强制内容类型为 HTML
+	router.Use(forceHTMLMiddleware)
+
+	// 通过命名路由获取 URL 示例
+	// homeURL, _ := router.Get("home").URL()
+	// fmt.Println("homeURL: ", homeURL)
+	// articleURL, _ := router.Get("articles.show").URL("id", "1")
+	// fmt.Println("articleURL: ", articleURL)
+
+	// 1.Gorilla Mux 会先匹配路由，再执行中间件，故使用中间件永远会返回 404. 
+	// 2.所以写一个函数把 Gorilla Mux 包起来，在这个函数中我们先对进来的请求做处理，然后再传给 Gorilla Mux 去解析。
+	http.ListenAndServe(":3000", removeTrailingSlash(router))
+}
+
+
+// 中间件
 func forceHTMLMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// 1. 设置标头
@@ -46,6 +99,7 @@ func forceHTMLMiddleware(next http.Handler) http.Handler {
 	})
 }
 
+// 请求进来时，删除掉  /
 func removeTrailingSlash(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// 1. 除首页以外，移除所有请求路径后面的斜杆
@@ -56,31 +110,4 @@ func removeTrailingSlash(next http.Handler) http.Handler {
 		// 2. 将请求传递下去
 		next.ServeHTTP(w, r)
 	})
-}
-
-func main() {
-	router := mux.NewRouter()
-
-	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
-	router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
-
-	router.HandleFunc("/articles/{id:[0-9]+}", articlesShowHandler).Methods("GET").Name("articles.show")
-	router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
-	router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
-
-	// 自定义 404 页面
-	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
-
-	// 中间件：强制内容类型为 HTML
-	router.Use(forceHTMLMiddleware)
-
-	// 通过命名路由获取 URL 示例
-	homeURL, _ := router.Get("home").URL()
-	fmt.Println("homeURL: ", homeURL)
-	articleURL, _ := router.Get("articles.show").URL("id", "1")
-	fmt.Println("articleURL: ", articleURL)
-
-	// 1.Gorilla Mux 会先匹配路由，再执行中间件，故使用中间件永远会返回 404. 
-	// 2.所以写一个函数把 Gorilla Mux 包起来，在这个函数中我们先对进来的请求做处理，然后再传给 Gorilla Mux 去解析。
-	http.ListenAndServe(":3000", removeTrailingSlash(router))
 }
