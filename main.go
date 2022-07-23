9bc61310145aaf1e6d50740ef39c180f73bf81b6
luke <kyloluke88@gmail.com>
2022-07-24 01:47:32 +0800

    表单验证

--- main.go
+++ main.go
@@ -3,10 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"net/url"
 	"strings"
+	"text/template"
+	"unicode/utf8"
 
 	"github.com/gorilla/mux"
 )
+
+// ArticlesFormData 创建博文表单数据
+type ArticlesFormData struct {
+	Title, Body string
+	URL         *url.URL
+	Errors      map[string]string
+}
+
 // router := mux.NewRouter()    包级别的变量声明时不能使用 := 语法
 var router = mux.NewRouter()
 
@@ -36,31 +47,95 @@ func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.PostFormValue("title")
+	body := r.PostFormValue("body")
 
+	// var errors  map[string]string
+	errors := make(map[string]string)
+	// 验证标题
+	if title == "" {
+		errors["title"] = "标题不能为空"
+	} else if utf8.RuneCountInString(title) < 3 || utf8.RuneCountInString(title) > 40 {
+		errors["title"] = "标题长度需介于 3-40"
+	}
+
+	if body == "" {
+		errors["body"] = "内容不能为空"
+	} else if utf8.RuneCountInString(body) < 10 {
+		errors["body"] = "内容长度需大于或等于 10 个字节"
+	}
+
+	// 检查是否有错误
+	if len(errors) == 0 {
+		fmt.Fprint(w, "验证通过!<br>")
+		fmt.Fprintf(w, "title 的值为: %v <br>", title) // %v 是专门用来打印字符串的么？？？
+		fmt.Fprintf(w, "title 的长度为: %v <br>", len(title))
+		fmt.Fprintf(w, "body 的值为: %v <br>", body)
+		fmt.Fprintf(w, "body 的长度为: %v <br>", len(body))
+	} else {
+		html := `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>创建文章 —— 我的技术博客</title>
+    <style type="text/css">.error {color: red;}</style>
+</head>
+<body>
+    <form action="{{ .URL }}" method="post">
+        <p><input type="text" name="title" value="{{ .Title }}"></p>
+        {{ with .Errors.title }}
+        <p class="error">{{ . }}</p>
+        {{ end }}
+        <p><textarea name="body" cols="30" rows="10">{{ .Body }}</textarea></p>
+        {{ with .Errors.body }}
+        <p class="error">{{ . }}</p>
+        {{ end }}
+        <p><button type="submit">提交</button></p>
+    </form>
+</body>
+</html>
+`
+		storeURL, _ := router.Get("articles.store").URL()
+
+		data := ArticlesFormData{
+			Title:  title,
+			Body:   body,
+			URL:    storeURL,
+			Errors: errors,
+		}
+		tmpl, err := template.New("create-form").Parse(html)
+		if err != nil {
+			panic(err)
+		}
+
+		err = tmpl.Execute(w, data)
+		if err != nil {
+			panic(err)
+		}
+	}
 	// 1. 直接获取指定的参数
 	// fmt.Fprintf(w, "r.Form 中 title 的值为: %v <br>", r.FormValue("title"))
-    // fmt.Fprintf(w, "r.PostForm 中 title 的值为: %v <br>", r.PostFormValue("title"))
-    // fmt.Fprintf(w, "r.Form 中 test 的值为: %v <br>", r.FormValue("test"))
-    // fmt.Fprintf(w, "r.PostForm 中 test 的值为: %v <br>", r.PostFormValue("test"))
+	// fmt.Fprintf(w, "r.PostForm 中 title 的值为: %v <br>", r.PostFormValue("title"))
+	// fmt.Fprintf(w, "r.Form 中 test 的值为: %v <br>", r.FormValue("test"))
+	// fmt.Fprintf(w, "r.PostForm 中 test 的值为: %v <br>", r.PostFormValue("test"))
 
 	// 2 获取所有的参数
-	fmt.Fprint(w, "创建新的文章")
-	err := r.ParseForm()
-    if err != nil {
-        // 解析错误，这里应该有错误处理
-        fmt.Fprint(w,  "请提供正确的数据！")
-        return
-    }
+	// err := r.ParseForm()
+	// if err != nil {
+	//     // 解析错误，这里应该有错误处理
+	//     fmt.Fprint(w,  "请提供正确的数据！")
+	//     return
+	// }
 
-    title := r.PostForm.Get("title")
+	// title := r.PostForm.Get("title")// 在使用之前需要调用 ParseForm 方法
 
-    fmt.Fprintf(w, "POST PostForm: %v <br>", r.PostForm)// PostForm：存储了 post、put 参数，在使用之前需要调用 ParseForm 方法。
-    fmt.Fprintf(w, "POST Form: %v <br>", r.Form)// Form：存储了 post、put 和 get 参数，在使用之前需要调用 ParseForm 方法。
-    fmt.Fprintf(w, "title 的值为: %v", title)
+	// fmt.Fprintf(w, "POST PostForm: %v <br>", r.PostForm)// PostForm：存储了 post、put 参数，在使用之前需要调用 ParseForm 方法。
+	// fmt.Fprintf(w, "POST Form: %v <br>", r.Form)// Form：存储了 post、put 和 get 参数，在使用之前需要调用 ParseForm 方法。
+	// fmt.Fprintf(w, "title 的值为: %v", title)
 }
 
 func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
-    html := `
+	html := `
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -75,8 +150,8 @@ func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
 </body>
 </html>
 `
-    storeURL, _ := router.Get("articles.store").URL()
-    fmt.Fprintf(w, html, storeURL)
+	storeURL, _ := router.Get("articles.store").URL()
+	fmt.Fprintf(w, html, storeURL)
 }
 
 func main() {
@@ -88,8 +163,6 @@ func main() {
 	router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
 	router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
 	router.HandleFunc("/articles/create", articlesCreateHandler).Methods("GET").Name("articles.create")
-
-
 
 	// 自定义 404 页面
 	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
@@ -103,11 +176,10 @@ func main() {
 	// articleURL, _ := router.Get("articles.show").URL("id", "1")
 	// fmt.Println("articleURL: ", articleURL)
 
-	// 1.Gorilla Mux 会先匹配路由，再执行中间件，故使用中间件永远会返回 404. 
+	// 1.Gorilla Mux 会先匹配路由，再执行中间件，故使用中间件永远会返回 404.
 	// 2.所以写一个函数把 Gorilla Mux 包起来，在这个函数中我们先对进来的请求做处理，然后再传给 Gorilla Mux 去解析。
 	http.ListenAndServe(":3000", removeTrailingSlash(router))
 }
-
 
 // 中间件
 func forceHTMLMiddleware(next http.Handler) http.Handler {
@@ -128,7 +200,7 @@ func removeTrailingSlash(next http.Handler) http.Handler {
 		}
 
 		// 2. 将请求传递下去
-		fmt.Print(r.URL.Path)
+		// fmt.Print(r.URL.Path)
 		next.ServeHTTP(w, r)
 	})
 }
