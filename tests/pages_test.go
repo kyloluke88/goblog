b47bf50ba4cceaa7c39894c4ffdb3ddf7ac4278b
luke <kyloluke88@gmail.com>
2022-07-28 23:39:05 +0800

    完成代码测试

--- tests/pages_test.go
+++ tests/pages_test.go
@@ -1,25 +1,52 @@
 package tests
 
 import (
-	"net/http"
-	"testing"
+    "net/http"
+    "strconv"
+    "testing"
 
-	"github.com/stretchr/testify/assert"
+    "github.com/stretchr/testify/assert"
 )
 
-func TestHomePage(t *testing.T) {
+func TestAllPages(t *testing.T) {
 
-	// 参考：https://learnku.com/courses/go-basic/1.17/refactoring-testing/11517
-	baseURL := "http://localhost:3000"
-	// 1. 请求，模拟用户访问浏览器
-	var (
-		resp *http.Response
-		err  error
-	)
+    baseURL := "http://localhost:3000"
 
-	//Go 的 http 包兼具 HTTP 服务器和 HTTP 客户端的功能，HTTP 客户端支持 GET/POST/PUT 等请求方式，常用于访问网页，或者请求第三方 API
-	resp, err = http.Get(baseURL + "/")
-	// 2. 检测 —— 是否无错误且 200
-	assert.NoError(t, err, "有错误发生，err 不为空")
-	assert.Equal(t, 200, resp.StatusCode, "应返回状态码 200")
+    // 1. 声明加初始化测试数据
+    var tests = []struct {
+        method   string
+        url      string
+        expected int
+    }{
+        {"GET", "/", 200},
+        {"GET", "/about", 200},
+        {"GET", "/notfound", 404},
+        {"GET", "/articles", 200},
+        {"GET", "/articles/create", 200},
+        {"GET", "/articles/3", 200},
+        {"GET", "/articles/3/edit", 200},
+        {"POST", "/articles/3", 200},
+        {"POST", "/articles", 200},
+        {"POST", "/articles/1/delete", 404},
+    }
+
+    // 2. 遍历所有测试
+    for _, test := range tests {
+        t.Logf("当前请求 URL: %v \n", test.url)
+        var (
+            resp *http.Response
+            err  error
+        )
+        // 2.1 请求以获取响应
+        switch {
+        case test.method == "POST":
+            data := make(map[string][]string)
+            resp, err = http.PostForm(baseURL+test.url, data)
+        default:
+            resp, err = http.Get(baseURL + test.url)
+        }
+        // 2.2 断言
+        assert.NoError(t, err, "请求 "+test.url+" 时报错")
+        assert.Equal(t, test.expected, resp.StatusCode, test.url+" 应返回状态码 "+strconv.Itoa(test.expected))
+    }
 }
