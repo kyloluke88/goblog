851adcd30f732668d7d9e6a260a02f7f9234a06a
luke <kyloluke88@gmail.com>
2022-08-03 02:07:13 +0800

    注册表单验证

--- app/http/controllers/auth_controller.go
+++ app/http/controllers/auth_controller.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"fmt"
 	"goblog/app/models/user"
+	"goblog/app/requests"
 	"goblog/pkg/view"
 	"net/http"
 )
@@ -18,24 +19,37 @@ func (*AuthController) Register(w http.ResponseWriter, r *http.Request) {
 
 // DoRegister 处理注册逻辑
 func (*AuthController) DoRegister(w http.ResponseWriter, r *http.Request) {
-	// 0. 初始化变量
-	name := r.PostFormValue("name")
-	email := r.PostFormValue("email")
-	password := r.PostFormValue("password")
-	// 1. 表单验证
-	// 2. 验证通过 —— 入库，并跳转到首页
-	_user := user.User{
-		Name:     name,
-		Email:    email,
-		Password: password,
-	}
-	_user.Create()
 
-	if _user.ID > 0 {
-		fmt.Fprint(w, "插入成功，ID 为"+_user.GetStringID())
-	} else {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprint(w, "创建用户失败，请联系管理员")
+	// 1. 初始化数据
+	_user := user.User{
+		Name:            r.PostFormValue("name"),
+		Email:           r.PostFormValue("email"),
+		Password:        r.PostFormValue("password"),
+		PasswordConfirm: r.PostFormValue("password_confirm"),
 	}
-	// 3. 表单不通过 —— 重新显示表单
+
+	// 2. 表单规则
+	errs := requests.ValidateRegistrationForm(_user)
+
+	if len(errs) > 0 {
+		// 3. 有错误发生，打印数据
+		// data, _ := json.MarshalIndent(errs, "", "  ")
+		// fmt.Fprint(w, string(data))
+
+		// 3. 表单不通过 —— 重新显示表单
+		view.RenderSimple(w, view.D{
+			"Errors": errs,
+			"User":   _user,
+		}, "auth.register")
+	} else {
+		// 4. 验证成功，创建数据
+		_user.Create()
+
+		if _user.ID > 0 {
+			http.Redirect(w, r, "/", http.StatusFound)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, "注册失败，请联系管理员")
+		}
+	}
 }
--- app/models/user/user.go
+++ app/models/user/user.go
@@ -2,10 +2,15 @@ package user
 
 import "goblog/app/models"
 
+// User 用户模型，数据验证同时可以用
 type User struct {
 	models.BaseModel
 
-	Name     string `gorm:"column:name;type:varchar(255);not null;unique"`
-    Email    string `gorm:"column:email;type:varchar(255);default:NULL;unique;"`
-    Password string `gorm:"column:password;type:varchar(255)"`
+	// 默认将键小写化作为字段名，所以下方的  column:name 等可以去除
+	// 默认是可以为null的，所以 default:NULL 也可以去除
+	Name     string `gorm:"column:name;type:varchar(255);not null;unique" valid:"name"`
+	Email    string `gorm:"type:varchar(255);default:NULL;unique;" valid:"email"`
+	Password string `gorm:"column:password;type:varchar(255)" valid:"password"`
+	// gorm:"-" —— 设置 GORM 在读写时略过此字段，仅用于表单验证
+	PasswordConfirm string ` gorm:"-" valid:"password_confirm"`
 }
