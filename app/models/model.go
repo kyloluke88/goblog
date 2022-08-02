4770efb94153ba97e0ef2ae880eba7ed8176086a
luke <kyloluke88@gmail.com>
2022-08-03 01:39:07 +0800

    register is successful

--- app/models/model.go
+++ app/models/model.go
@@ -2,11 +2,14 @@ package models
 
 import (
 	"goblog/pkg/types"
+	"time"
 )
 
 // BaseModel 模型基类
 type BaseModel struct {
-	ID uint64
+	ID        uint64
+	CreatedAt time.Time `gorm:"column:created_at;index"`
+	UpdatedAt time.Time `gorm:"column:updated_at;index"`
 }
 
 // GetStringID 获取 ID 的字符串格式
