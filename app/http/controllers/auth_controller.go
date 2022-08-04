5d3a9b59d97fd592d6389e1d0360f584fae364e5
luke <kyloluke88@gmail.com>
2022-08-04 20:37:28 +0800

    add flash message by session

--- app/http/controllers/auth_controller.go
+++ app/http/controllers/auth_controller.go
@@ -5,6 +5,7 @@ import (
 	"goblog/app/models/user"
 	"goblog/app/requests"
 	"goblog/pkg/auth"
+	"goblog/pkg/flash"
 	"goblog/pkg/view"
 	"net/http"
 )
@@ -47,6 +48,7 @@ func (*AuthController) DoRegister(w http.ResponseWriter, r *http.Request) {
 		_user.Create()
 
 		if _user.ID > 0 {
+			flash.Success("恭喜您注册成功！")
 			auth.Login(_user)
 			http.Redirect(w, r, "/", http.StatusFound)
 		} else {
@@ -66,6 +68,7 @@ func (*AuthController) DoLogin(w http.ResponseWriter, r *http.Request) {
 
 	if err := auth.Attempt(email, password); err == nil {
 		// 登录成功
+		flash.Success("欢迎回来！")
 		http.Redirect(w, r, "/", http.StatusFound)
 	} else {
 		// 登录失败
@@ -80,5 +83,6 @@ func (*AuthController) DoLogin(w http.ResponseWriter, r *http.Request) {
 // 退出登录
 func (*AuthController) Logout(w http.ResponseWriter, r *http.Request) {
 	auth.Logout()
+	flash.Success("您已退出登录")
 	http.Redirect(w, r, "/", http.StatusFound)
 }
---
+++ pkg/flash/flash.go
@@ -0,0 +1,60 @@
+package flash
+
+import (
+	"encoding/gob"
+	"goblog/pkg/session"
+)
+
+// Flashes Flash 消息数组类型，用以在会话中存储 map
+type Flashes map[string]interface{}
+
+// 存入会话数据里的 key
+var flashKey = "_flashes"
+
+func init() {
+	// 在 gorilla/sessions 中存储 map 和 struct 数据需
+	// 要提前注册 gob，方便后续 gob 序列化编码、解码
+	//标准库 gob 是 Go 专属的编解码方式，是标准库自带的一个数据结构序列化的编码 / 解码工具。类似于 JSON 或 XML，不过执行效率比他们更高。特别适合在 Go 语言程序间传递数据。
+	gob.Register(Flashes{})
+}
+
+// Info 添加 Info 类型的消息提示
+func Info(message string) {
+	addFlash("info", message)
+}
+
+// Warning 添加 Warning 类型的消息提示
+func Warning(message string) {
+	addFlash("warning", message)
+}
+
+// Success 添加 Success 类型的消息提示
+func Success(message string) {
+	addFlash("success", message)
+}
+
+// Danger 添加 Danger 类型的消息提示
+func Danger(message string) {
+	addFlash("danger", message)
+}
+
+// All 获取所有消息
+func All() Flashes {
+	val := session.Get(flashKey)
+	// 读取时必须做类型检测
+	flashMessages, ok := val.(Flashes)
+	if !ok {
+		return nil
+	}
+	// 读取即销毁，直接删除
+	session.Forget(flashKey)
+	return flashMessages
+}
+
+// 私有方法，新增一条提示
+func addFlash(key string, message string) {
+	flashes := Flashes{}
+	flashes[key] = message
+	session.Put(flashKey, flashes)
+	// session.Save()
+}
