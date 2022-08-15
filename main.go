be53cddc92c8842063ef8ed9e51afce308335fdb
luke <kyloluke88@gmail.com>
2022-08-16 01:33:51 +0800

    打包模板文件.gohtml

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 // 关于包中的init函数  查阅：https://learnku.com/go/t/47178
 import (
+	"embed"
 	"goblog/app/http/middlewares"
 	"goblog/bootstrap"
 	"goblog/config"
@@ -9,6 +10,12 @@ import (
 	"goblog/pkg/logger"
 	"net/http"
 )
+
+//go:embed resources/views/articles/*
+//go:embed resources/views/auth/*
+//go:embed resources/views/categories/*
+//go:embed resources/views/layouts/*
+var tplFS embed.FS
 
 func init() {
 	// 初始化配置信息
@@ -18,6 +25,10 @@ func init() {
 func main() {
 
 	bootstrap.SetupDB()
+
+	// 初始化模板
+	bootstrap.SetupTemplate(tplFS)
+
 	router := bootstrap.SetupRoute()
 
 	err := http.ListenAndServe(":"+c.GetString("app.port"), middlewares.RemoveTrailingSlash(router))
